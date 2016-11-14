914e753710f758b2b3e1c8607265468fcf6271cc
robzd1 <robzd1@gmail.com>
2016-11-14 04:02:35 +0000

    purchase action and confirmation page

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -22,8 +22,26 @@ class CardsController < ApplicationController
   
   
   def purchase
-    nonce = params[:payment_method_nonce]
-  
+    
+    neworder = Order.new(
+      card_id: params[:id],
+      address_line_1: params[:address_line_1],
+      address_line_2: params[:address_line_2],
+      city: params[:city],
+      state_abbr: params[:state_abbr],
+      zip_code: params[:zip_code]
+    )
+
+    
+    result = Braintree::Transaction.sale(
+      :amount => params[:price],
+      :payment_method_nonce => params[:payment_method_nonce],
+      :options => {
+        :submit_for_settlement => true
+      }
+    )
+    
+    render :confirmation, locals: {zone: "test"}
   
   end
   
--- db/migrate/20161111204912_create_orders.rb
+++ db/migrate/20161111204912_create_orders.rb
@@ -2,6 +2,7 @@ class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
         t.string :card_id
+        t.string :name
         t.string :address_line_1
         t.string :address_line_2
         t.string :city
