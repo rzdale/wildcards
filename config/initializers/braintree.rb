2d816bfac666ee375eb673f445ba2914c500e576
Rob Dale <robzd1@gmail.com>
2016-11-14 18:50:06 -0600

    Delete braintree.rb

--- config/initializers/braintree.rb
+++
@@ -1,4 +0,0 @@
-Braintree::Configuration.environment = :sandbox
-Braintree::Configuration.merchant_id = "q979gnj3b96ctzp8"
-Braintree::Configuration.public_key = "zgj422ysqgg7gchy"
-Braintree::Configuration.private_key = "694d0aed33c94614083ce373db626d4f"
